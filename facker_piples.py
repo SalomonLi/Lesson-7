ec4f8b91d7a4a6e269f5d41ce6b8f8890c968fe1
Larry Salomon <salomonlarry@gmail.com>
2021-12-30 00:27:49 +0100

    I dont know (@property.setter)

--- facker_piples.py
+++ facker_piples.py
@@ -1,7 +1,7 @@
 import time
 from faker import Faker
 
-fake = Faker()
+
 
 def test_time(func):
     def wrapper(*args, **kwargs):
@@ -13,25 +13,23 @@ def test_time(func):
         return res
 
     return wrapper
-
-
-@test_time
-def get_nod(a , b):
-    while a != b:
-        if a > b:
-            a -= b
-        else:
-            b -= a
-    return a
-
-
-@test_time
-def get_fast_nod(a, b):
-    if a < b:
-        a, b = b, a
-    while b != 0:
-        a, b = b, a % b
-    return a
+# @test_time
+# def get_nod(a , b):
+#     while a != b:
+#         if a > b:
+#             a -= b
+#         else:
+#             b -= a
+#     return a
+#
+#
+# @test_time
+# def get_fast_nod(a, b):
+#     if a < b:
+#         a, b = b, a
+#     while b != 0:
+#         a, b = b, a % b
+#     return a
 
 # get_nod = test_time(get_nod)
 # get_fast_nod = test_time(get_fast_nod)
@@ -43,74 +41,112 @@ def get_fast_nod(a, b):
 #     print(False)
 # print(res, res2)
 class Night_Club:
-   def __init__(self, first_name, last_name, company_name, job, email):
+
+    def __init__(self, first_name, last_name, company_name, job, email):
        self.first_name = first_name
        self.last_name = last_name
        self.company_name = company_name
        self.job = job
        self.email = email
-# def test_users(n=50):
-#     fake = Faker(locale='de_DE')
-#     fake.seed(42)
-#
-#     users = []
-#     for i in xrange(n):
-#         u = User()
-#         u.first_name = fake.first_name()
-#         u.last_name = fake.last_name()
-#         u.email_address = fake.email()
-#         u.password = u.original_password = fake.password()
-#         u.tracking_key = fake.random_number(digits=6)
-#
-#         users.append(u)
-#
-#     return users
+       # self._values = len((self.first_name + " " + self.last_name))
 
-def valueset(self, nbvalues):
-    """
-    Generate the values
-    """
-    values = []
 
-    f = Faker()
-    for i in range(1,2):
-        values.append({"company": f.company(),
-                       "name": f.name()[:300],
-                       "street_address": f.street_address()[:300],
-                       "email": "{}".format( f.email()),
-                       "value": f.random_number(),
-                       "vali": f.random_number(),
-                       "country": f.country()[:100],
-                       "latitude": f.latitude(),
-                       "longitude": f.longitude(),
-                       "city": f.city()[:50],
-                       "city_suffix": f.city_suffix()[:20],
-                       "locale": f.locale()[:8],
-                       })
 
-    return values
-@test_time
+    @property
+    def lens(self):
+        # lens = f'{self.first_name} {self.last_name}'
+        # self._values = len(lens)
+        x = len((self.first_name + " " + self.last_name))
+        contact = f'Contact to "{self.first_name} {self.last_name} {self.email}"'
+        # print(contact)
+        return x
+
+    @lens.setter
+    def contact(self):
+        pass
+        # print(values)
+        # contact = f'Contact to "{self.first_name} {self.last_name} {self.email}"'
+        # print(contact)
+        # return contact
+
+
+
+
 def generate():
-    fake = Faker()
+   fake = Faker()
 
-    first_name = fake.first_name()
-    last_name = fake.last_name()
-    company_name = fake.company()
-    job = fake.job()
-    email = fake.email()
-    # addr = fake.street_address()
-    # city = cities.get_random()
-    # phone = fake.random_number(digits=10)
-    # license = ''.join(random.choice('0123456789ABCDEF') for i in range(8))
+   first_name = fake.first_name()
+   last_name = fake.last_name()
+   company_name = fake.company()
+   job = fake.job()
+   email = fake.email()
+   # addr = fake.street_address()
+   # city = cities.get_random()
+   # phone = fake.random_number(digits=10)
+   # license = ''.join(random.choice('0123456789ABCDEF') for i in range(8))
+
+   night_club = Night_Club(first_name, last_name, company_name, job, email)
+   return night_club
+
+def generate_dict():
+   resul = []
+   for i in range(1, 100):
+       fake_agent = generate()
+       listing.append({i: {"last_name": fake_agent.last_name, "first_name": fake_agent.first_name}})
+       # print(f"First Name- {fake_agent.first_name}, Last Name- {fake_agent.last_name}, "
+       #   f"Company- {fake_agent.company_name}, Job- {fake_agent.job}, Email- {fake_agent.email}")
+   return resul
 
 
-    night_club = Night_Club(first_name, last_name, company_name, job, email)
-    return night_club
+client_id_0 = generate()
+client_id_1 = generate()
+client_id_2 = generate()
+client_id_3 = generate()
+client_id_4 = generate()
+client_id_5 = generate()
 
-fake_agent = generate()
-fake_agent2 = generate()
 
-for i in range(1, 100):
-    fake_agent = generate()
-    print(f"First Name- {fake_agent.first_name}, Last Name- {fake_agent.last_name}, "
-      f"Company- {fake_agent.company_name}, Job- {fake_agent.job}, Email- {fake_agent.email}")
+print(type(client_id_0.contact))
+x=client_id_0.contact
+print(x)
+
+client_list = [client_id_0, client_id_1, client_id_2, client_id_3, client_id_4, client_id_5]
+
+# print(client_list)
+cliets_first_name_sorted = sorted(client_list, key=lambda client_id: client_id.first_name)
+cliets_last_name_sorted = sorted(client_list, key=lambda client_id: client_id.last_name)
+cliets_email_sorted = sorted(client_list, key=lambda client_id: client_id.email)
+# print('',cliets_first_name_sorted, '\n', cliets_last_name_sorted, '\n', cliets_email_sorted)
+
+
+# print(len(client_id_0))
+
+# # Using @property decorator
+# class Celsius:
+#     def __init__(self, temperature=0):
+#         self.temperature = temperature
+#
+#     def to_fahrenheit(self):
+#         return (self.temperature * 1.8) + 32
+#
+#     @property
+#     def temperature(self):
+#         print("Getting value...")
+#         return self._temperature
+#
+#     @temperature.setter
+#     def temperature(self, value):
+#         print("Setting value...")
+#         if value < -273.15:
+#             raise ValueError("Temperature below -273 is not possible")
+#         self._temperature = value
+#
+#
+# # create an object
+# human = Celsius(37)
+#
+# print(human.temperature)
+#
+# print(human.to_fahrenheit())
+#
+# coldest_thing = Celsius(-300)
