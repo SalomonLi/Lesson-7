aa4717c075f5866df180e45fad21511657f24299
Larry Salomon <salomonlarry@gmail.com>
2022-01-07 21:23:14 +0100

    v.0_2

--- Data_Base_Films.py
+++ Data_Base_Films.py
@@ -1,4 +1,6 @@
-class Movie():
+import random
+
+class Movie:
     def __init__(self, title, year, genres, played = 0):
         self.title = title
         self.year = year
@@ -12,15 +14,22 @@ class Movie():
     def __str__(self):
         return f'{self.title} ({self.year})'
 
+
+
+
 class Selial(Movie):
-    def __init__(self, number_episode, number_seasons, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self,number_episode, number_seasons, title, year, genres, played = 0, *args, **kwargs):
+        super().__init__(title, year, genres,played = 0,)
+        self.title = title
+        self.year = year
+        self.genres = genres
+        self.played = played
         self.number_episode = number_episode
         self.number_seasons = number_seasons
 
 
     def __str__(self):
-        return f'{self.title} S{self.number_seasons:0>2}E{self.number_episode:0>2}'
+        return f'{self.title} S{self.number_seasons:0>3}E{self.number_episode:0>2}'
 
 
 film = Movie(title="Flach", year= 2000, genres="cos")
@@ -36,3 +45,58 @@ serials = Selial(title="The Simpsons", year=1999, genres="cos", number_episode=1
 
 print(film)
 print(serials)
+
+x = str(serials)
+#
+# print(type(x))
+# print(x)
+def get_movies(values):
+    rezultat = []
+    if isinstance(values, Selial) != True :
+        rezultat.append(str(values))
+        return rezultat
+
+
+
+def get_serials(values):
+    rezultat = []
+    if isinstance(values, Selial):
+        rezultat.append(str(values))
+        return rezultat
+
+def search(values, object):
+    rezultat = []
+    for searching in values:
+        if str(searching).lower() in str(object).lower():
+            rezultat.append(searching.title())
+    return rezultat
+
+def generate_views(object):
+    x = random.choice(object)
+    if isinstance(x, Movie):
+        return x.title
+
+def top_titles(*content_type):
+    # nie wiem jak to zric
+    pass
+
+
+
+
+# x = get_movies(film)
+# print(x)
+# x = get_movies(serials)
+# print(x)
+
+print(get_movies(film))
+print(get_movies(serials))
+
+print(get_serials(film))
+print(get_serials(serials))
+
+# print(search(('Flach', 'Grezbi', "..."), serials))
+print(search(('Flach', 'Grezbi', "...", "flach"), film))
+
+x = (film, serials)
+
+print(generate_views(x))
