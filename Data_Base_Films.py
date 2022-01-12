386f896421edccad854a25a1d369b188c2b282ee
Larry Salomon <salomonlarry@gmail.com>
2022-01-12 23:01:11 +0100

    v.0_4

--- Data_Base_Films.py
+++ Data_Base_Films.py
@@ -1,4 +1,5 @@
 import random
+from typing import List, Type
 
 class Movie:
     def __init__(self, title, year, genres, played = 0):
@@ -17,7 +18,7 @@ class Movie:
 
 
 
-class Selial(Movie):
+class Serials(Movie):
     def __init__(self,number_episode, number_seasons, title, year, genres, played = 0, *args, **kwargs):
         super().__init__(title, year, genres,played = 0,)
         self.title = title
@@ -40,55 +41,59 @@ film_id_4 = Movie(title="Pulp Fiction", year= 2000, genres="cos")
 film_id_5 = Movie(title="Star Wars", year= 2000, genres="cos")
 
 
-serials = Selial(title="The Simpsons", year=1999, genres="cos", number_episode=1, number_seasons=18)
+serials = Serials(title="The Simpsons", year=1999, genres="cos", number_episode=1, number_seasons=18)
 
 films = [film_id_0, serials, film_id_1, film_id_2, film_id_3, film_id_4, film_id_5]
 
 
 
 def get_movies(values):
-    rezultat = []
-    for name in values:
-        if isinstance(name, Selial) != True :
-            rezultat.append(name)
-    return sorted(rezultat, key= lambda films_id: films_id.title)
+    rezul = []
+    for loop__elements_item in values:
+        if type(loop__elements_item) is Movie:
+            rezul.append(loop__elements_item)
+    return sorted(rezul, key=lambda films_id: films_id.title)
 
 
 
 def get_serials(values):
-    rezultat = []
-    for name in values:
-        if isinstance(name, Selial):
-            rezultat.append(name)
-    return sorted(rezultat, key= lambda films_id: films_id.title)
+    result = []
+    for loop__elements_item in values:
+        if type(loop__elements_item) is Serials:
+            result.append(loop__elements_item)
+    return sorted(result, key=lambda films_id: films_id.title)
 
 def search(values, object_type_class):
-    rezultat = []
-    for searching in values:
-        for parser_class_obj in object_type_class:
-            if str(searching).lower() in str(parser_class_obj).lower():
-                rezultat.append(searching.title())
-    return rezultat
+    result = []
+    for loop_0_elements_item in values:
+        for loop_1_elements_item in object_type_class:
+            if str(loop_0_elements_item).lower() in str(loop_1_elements_item).lower():
+                result.append(loop_0_elements_item.title())
+    return result
 
 def generate_views(object_types_list):
     random_values = random.choice(object_types_list)
     random_quantity = random.randint(1, 100)
     if isinstance(random_values, Movie):
         random_values.played = random_quantity
-        return f'{random_values} Quantity = {random_values.played} '
+        return f'{random_values} Quantity = {random_values.played}'
 
 
 def generate_views_loop(value = 0):
     for _ in range(0, value):
         generate_views(films)
 
-def top_titles(values, top = 3, content_type = films or serials):
-
-    top_sorted = sorted(values , key = lambda values : values.played)
-    if top <= len(top_sorted):
+def top_titles(values: List[Movie], content_type: Type[Movie or Serials], top: int = 3):
+    if content_type == Movie:
+        top_sorted = sorted(get_movies(values), key=lambda values: values.played, reverse=True)
+        return top_sorted[:top]
+    if content_type == Serials:
+        top_sorted = sorted(get_serials(values), key=lambda values: values.played, reverse=True)
         return top_sorted[:top]
     else:
-        return top_sorted
+        top_sorted = sorted(values, key=lambda values: values.played, reverse=True)
+        return top_sorted[:top]
+
 
 
 
@@ -108,5 +113,10 @@ generate_views(films)
 generate_views_loop(10)
 
 
-top_list = top_titles(films)
+top_list = top_titles(films, None)
+top_list_movies = top_titles(films, Movie)
+top_list_serials = top_titles(films, Serials)
+
 print(top_list)
+print(top_list_movies)
+print(top_list_serials)
